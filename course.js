3b7ac3b58ac58eb958a41f736fc4dc242dc66ad2
karpenberg <karpenberg@gmail.com>
2018-04-28 11:54:49 +0300

    first

---
+++ course.js
@@ -0,0 +1,27 @@
+﻿class Course {
+    constructor() {
+        var sergey = new Student('sergey', 'karpovich', '01/20/1993', [90, 60, 90]);
+        var andrey = new Student('karpovich', 'andrey', '03/28/1986', [90, 90, 90]);       
+
+        sergey.present();
+        sergey.present();
+        sergey.absent();
+
+        andrey.present();
+        andrey.present();
+        andrey.present();      
+
+        this.students = [sergey, andrey];
+    }
+    attendance(lastName) {
+        if (!arguments.length) {
+            return this.students.reduce((r,s) => r + s.avaragePresence, 0) / this.students.length;
+        }
+        return this.students.sort(function (a, b) { return b.avaragePresence - a.avaragePresence }).findIndex(s => s.lastName === lastName) + 1;
+    }
+}
+
+var course = new Course();
+
+console.log(course.attendance());
+console.log(course.attendance('karpovich'));
---
+++ student.js
@@ -0,0 +1,65 @@
+﻿class Student {
+    constructor(firstName, lastName, birthDay, marks) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.marks = marks;
+        this.absence = new Array(25);
+        this.absenceIndex = 0;
+        this.age = this.getAge(birthDay);
+        this.averageMark = this.marks.reduce((r, m) => r + m) / this.marks.length;
+        this.presenceFactor = 0.9;
+        this.goodMarksMin = 90;
+        this.results = {
+            BAD: "Редиска!",
+            NORMAL: "Норм, но можно лучше",
+            GOOD: "Ути какой молодчинка!"
+        };
+    }
+
+       absent() {
+        if (this.absence.length > this.absenceIndex) {
+            this.absence[this.absenceIndex] = false;
+            this.absenceIndex++;
+        }
+    };
+
+    present() {
+        if (this.absence.length > this.absenceIndex) {
+            this.absence[this.absenceIndex] = true;
+            this.absenceIndex++;
+        }
+    };
+
+    get avaragePresence(){
+        var precenceCount = this.absence.slice(0, this.absenceIndex).filter(x => x).length;
+        return precenceCount / this.absenceIndex;
+    }
+
+    get avarageMark(){
+        return this.avarageMark;
+    }
+
+    getAge(dateString) {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    summary() {
+        
+        if (this.averageMark < this.goodMarksMin && this.avaragePresence < this.presenceFactor) {
+            console.log(this.results.BAD);
+        } else if (this.averageMark < this.goodMarksMin || this.avaragePresence < this.presenceFactor)
+            console.log(this.results.NORMAL);
+        else
+            console.log(this.results.GOOD);
+    }   
+}
+
+
